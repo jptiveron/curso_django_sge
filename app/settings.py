b045e49543a4d52bc766344110b82c5239fa02cb
jptiveron <jptiveron@hotmail.com>
2025-08-05 11:25:58 -0300

    model categories created

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'brands.apps.BrandsConfig'
+    'brands.apps.BrandsConfig',
+    'categories.apps.CategoriesConfig'
 ]
 
 MIDDLEWARE = [
