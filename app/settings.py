3e0e7f485898aeed529e047562ec08780e9e54d6
jptiveron <jptiveron@hotmail.com>
2025-08-05 11:28:18 -0300

    model suppliers created

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'brands.apps.BrandsConfig',
-    'categories.apps.CategoriesConfig'
+    'categories.apps.CategoriesConfig',
+    'suppliers.apps.SuppliersConfig'
 ]
 
 MIDDLEWARE = [
